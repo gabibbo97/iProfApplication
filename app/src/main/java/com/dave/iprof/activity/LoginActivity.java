26c9dd4ef5f8329aa119badc7a23e7edc826b2a0
Davide Caputo <dave.caputo93@gmail.com>
2019-05-10 10:48:42 +0200

    Initial commit

---
+++ app/src/main/java/com/dave/iprof/activity/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.dave.iprof.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dave.iprof.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
